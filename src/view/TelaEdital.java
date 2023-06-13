51fd5a38e10220cadde96df6873c0a8cc23d7197
Camila <Camila@DESKTOP-98B499J>
2023-06-13 16:28:57 -0300

    TelaEdital 80% feita

--- src/view/TelaEdital.java
+++ src/view/TelaEdital.java
@@ -1,67 +1,117 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class TelaEdital extends JFrame {
-    public TelaEdital() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Estudo para Concurso | EDITAIS");
+class ItemLista {
+    private String nome;
+    private Date data;
 
-        JButton btnEdital1 = new JButton("Edital na data Atual");
-        btnEdital1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String dataInserida = JOptionPane.showInputDialog("Digite a data ATUAL (DD/MM/AAAA):");
-
-                // Obtendo a data atual
-                LocalDate dataAtual = LocalDate.now();
-
-                // Convertendo a data inserida em LocalDate
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                LocalDate dataInseridaFormatada = LocalDate.parse(dataInserida, formatter);
-
-                // Comparando as datas
-                if (dataInseridaFormatada.equals(dataAtual)) {
-                    JOptionPane.showMessageDialog(null, "Edital está válido!");
-                } else {
-                    JOptionPane.showMessageDialog(null, "Edital está inválido!");
-                }
-            }
-        });
-
-        JButton btnEdital2 = new JButton("Editais Gerais");
-        btnEdital2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Lógica para abrir a outra tela com a lista de editais completos
-                // Implemente a lógica de abertura da nova tela aqui
-                // Você pode criar outra classe que estende JFrame para representar a nova tela
-            }
-        });
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        panel.add(btnEdital1);
-        panel.add(btnEdital2);
-
-        getContentPane().add(panel);
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
+    public ItemLista(String nome, Date data) {
+        this.nome = nome;
+        this.data = data;
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new TelaEdital();
-            }
-        });
+    public String getNome() {
+        return nome;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public String getFormattedDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        return dateFormat.format(data);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - " + getFormattedDate();
     }
 }
 
+public class TelaEdital {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Estudo para Concurso | EDITAIS");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Cria um array de objetos ItemLista com nome e data
+        ItemLista[] itens = {
+                new ItemLista("Banco do Brasil", new Date(2023, 5, 13)),
+                new ItemLista("Polícia Federal", new Date(2023, 6, 25)),
+                new ItemLista("DETRAN", new Date(2023, 9, 18)),
+                new ItemLista("Ministério da Fazenda", new Date(2023, 1, 3)),
+                new ItemLista("INSS", new Date(2023, 11, 25))
+        };
+
+        // Cria uma JList com o array de itens
+        JList<ItemLista> lista = new JList<>(itens);
+
+        // Define o modo de seleção para permitir seleção de vários itens
+        lista.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+        // Define um renderer personalizado para exibir apenas a data formatada dos itens
+        lista.setCellRenderer(new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index,
+                                                          boolean isSelected, boolean cellHasFocus) {
+                Component renderer = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                if (renderer instanceof JLabel && value instanceof ItemLista) {
+                    ItemLista item = (ItemLista) value;
+                    ((JLabel) renderer).setText(item.getNome() + " - " + item.getFormattedDate());
+                }
+                return renderer;
+            }
+        });
+
+        // Adiciona a JList a um JScrollPane para permitir a rolagem, se necessário
+        JScrollPane scrollPane = new JScrollPane(lista);
+
+        // Adiciona o JScrollPane ao frame
+        frame.getContentPane().add(scrollPane);
+
+        // Cria os botões
+        JButton button1 = new JButton("Ler");
+        JButton button2 = new JButton("Download");
+        JButton button3 = new JButton("Criar");
+        JButton button4 = new JButton("Deletar");
+
+        // Cria o painel para os botões
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(button1);
+        buttonPanel.add(button2);
+        buttonPanel.add(button3);
+        buttonPanel.add(button4);
+
+        // Adiciona o painel de botões ao frame
+        frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+
+        // Adiciona um ListSelectionListener para capturar os eventos de seleção
+        lista.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    // Verifica se algum item foi selecionado
+                    if (lista.getSelectedIndex() != -1) {
+                        // Exibe os botões
+                        buttonPanel.setVisible(true);
+                    } else {
+                        // Oculta os botões
+                        buttonPanel.setVisible(false);
+                    }
+                }
+            }
+        });
+        
+        
+
+        // Define o tamanho do frame e o torna visível
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+}
